fa87fddc8cc33c560ce712e930bdc6cf3f4b0337
Sebastian Rojas <sebastian.rojas6@unmsm.edu.pe>
2025-04-13 22:57:24 -0500

    New structure

---
+++ target/debug/deps/deligo-3150e925bc372821.d
@@ -0,0 +1,5 @@
+/home/sebastianrojas/Escritorio/lopez_project/target/debug/deps/libdeligo-3150e925bc372821.rmeta: src/main.rs
+
+/home/sebastianrojas/Escritorio/lopez_project/target/debug/deps/deligo-3150e925bc372821.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/deligo-f93637b2fa0e7f77.d
@@ -0,0 +1,5 @@
+/home/sebastianrojas/Escritorio/lopez_project/target/debug/deps/libdeligo-f93637b2fa0e7f77.rmeta: src/main.rs
+
+/home/sebastianrojas/Escritorio/lopez_project/target/debug/deps/deligo-f93637b2fa0e7f77.d: src/main.rs
+
+src/main.rs:
