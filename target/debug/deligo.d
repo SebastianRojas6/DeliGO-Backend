8efed6de0130f8ff618c7b13a543849b19f9bf48
Sebastian Rojas <sebastian.rojas6@unmsm.edu.pe>
2025-04-13 22:05:20 -0500

    Init commit

---
+++ target/debug/deligo.d
@@ -0,0 +1,1 @@
+/home/sebastianrojas/Escritorio/lopez_project/target/debug/deligo: /home/sebastianrojas/Escritorio/lopez_project/src/main.rs
---
+++ target/debug/deps/deligo-350700a3186f9234.d
@@ -0,0 +1,5 @@
+/home/sebastianrojas/Escritorio/lopez_project/target/debug/deps/deligo-350700a3186f9234: src/main.rs
+
+/home/sebastianrojas/Escritorio/lopez_project/target/debug/deps/deligo-350700a3186f9234.d: src/main.rs
+
+src/main.rs:
