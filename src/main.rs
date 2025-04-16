8730da2ade255811191f5fa468bc9b4d7bab2d77
miguelespp <carlos.espinoza24@unmsm.edu.pe>
2025-04-16 09:53:31 -0500

    feat: update order and product management with new CRUD operations and data structures. Not enjoy work with bigdecimal

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,29 @@ mod routes;
 mod db;
 
 use actix_web::{web, App, HttpServer};
-use crate::db::init_db;
-use crate::routes::{orders, products};
+use sqlx::PgPool;
+use crate::routes::orders::OrderController;
+use crate::routes::products::ProductController;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
-    let pool = init_db().await.expect("Failed to connect to database");
+    let database_url = "postgres://pguser:pgpassword@localhost/pgdatabase";
+    let pool = PgPool::connect(&database_url).await.unwrap();
 
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
-            .configure(orders::config)
-            .configure(products::config)
+            .route("/orders", web::post().to(OrderController::create_order))
+            .route("/orders", web::get().to(OrderController::get_all_orders))
+            .route("/orders/{id}", web::get().to(OrderController::get_order_by_id))
+            .route("/orders/{id}/status", web::put().to(OrderController::update_order_status))
+            .route("/orders/{id}", web::delete().to(OrderController::delete_order))
+            .route("/products", web::post().to(ProductController::create_product))
+            .route("/products", web::get().to(ProductController::get_all_products))
+            .route("/products/{id}", web::get().to(ProductController::get_product_by_id))
+            .route("/products/{id}", web::put().to(ProductController::update_product))
+            .route("/products/{id}", web::delete().to(ProductController::delete_product))
     })
         .bind("127.0.0.1:8080")?
         .run()
