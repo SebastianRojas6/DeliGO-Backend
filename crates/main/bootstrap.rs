c02285665e48f5e2c16f53f64455d92f33719bcd
SebastianRojas6 <sebastian.rojas6@unmsm.edu.pe>
2025-07-05 12:29:59 -0500

    Feat: orders_billing updated

--- crates/main/bootstrap.rs
+++ crates/main/bootstrap.rs
@@ -1,9 +1,11 @@
 use std::sync::Arc;
-use admin::crud_product::domain::repository::ProductAbstractRepository;
-use admin::crud_product::infrastructure::spi::db::db_product_facts_repository::DbProductFactsRepository;
 use shared::config::connect_to_supabase;
 
-use std::sync::Arc;
+use admin::crud_product::domain::repository::ProductAbstractRepository;
+use admin::crud_product::infrastructure::spi::db::db_product_facts_repository::DbProductFactsRepository;
+
+use orders_billing::payment_record::domain::repository::PaymentRepository;
+use orders_billing::payment_record::infrastructure::repository::SupabaseOrdersBillingRepository;
 
 use register_login::credential_validation::infrastructure::repository::SeaOrmUserCredentialRepository;
 use register_login::credential_validation::domain::repository::UserCredentialRepository;
@@ -13,7 +15,7 @@ pub struct AppState {
     pub jwt_secret: String,
     pub products_repo: Arc<dyn ProductAbstractRepository>,
     pub credential_repo: Arc<dyn UserCredentialRepository>,
-
+    pub payment_repo: Arc<dyn PaymentRepository>,
 }
 
 pub async fn init_state() -> Result<AppState, Box<dyn std::error::Error + Send + Sync>> {
@@ -22,12 +24,13 @@ pub async fn init_state() -> Result<AppState, Box<dyn std::error::Error + Send +
         .expect("Falta la variable SUPABASE_JWT_SECRET");
     
     let products_repo = Arc::new(DbProductFactsRepository::new( db.clone()));
-
-    let credential_repo = Arc::new(SeaOrmUserCredentialRepository { db });
+    let credential_repo = Arc::new(SeaOrmUserCredentialRepository { db : db.clone() });
+    let payment_repo: Arc<SupabaseOrdersBillingRepository> = Arc::new(SupabaseOrdersBillingRepository::new(db.clone()));
 
     Ok(AppState {
         jwt_secret: supabase_jwt_secret,
         products_repo,
         credential_repo,
+        payment_repo,
     })
 }
--- crates/orders_billing/payment_record/application/register_payment.rs
+++ crates/orders_billing/payment_record/application/register_payment.rs
@@ -19,7 +19,6 @@ impl RegisterPaymentUseCase {
         total_amount: f64,
         method: PaymentMethod,
     ) -> Result<(), String> {
-        // Verificar que no exista un pago ya registrado para el pedido
         if self.repo.get_by_order_id(order_id).await?.is_some() {
             return Err("Ya existe un pago para este pedido.".to_string());
         }
